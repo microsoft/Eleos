711d26d09c4791fc1e4e5a159b68114f28df8d23
ddrapp1 <daniel.drapp@mercy.net>
2024-07-29 11:32:56 -0500

    Added basic react app with chat pop-up integration

---
+++ apim-chat-portal/src/components/chat/MessageLoader.tsx
@@ -0,0 +1,13 @@
+import './MessageLoader.css';
+
+const MessageLoader = () => {
+    return (
+        <div className="message-loader-2">
+            <div className="loader-bar-2"></div>
+            <div className="loader-bar-2"></div>
+            <div className="loader-bar-2"></div>
+        </div>
+    );
+};
+
+export default MessageLoader;
